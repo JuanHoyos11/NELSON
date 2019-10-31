260ca0a310c52099154d5fe5d6c13913a502b8bd
Juan <juanhoyos11@hotmail.com>
2019-10-31 09:40:59 -0500

    TABLA

--- Proyecto 3/src/model/Cliente.java
+++ Proyecto 3/src/model/Cliente.java
@@ -36,6 +36,6 @@ public class Cliente {
 		this.telefonoContacto = telefonoContacto;
 	}
 
-
+       
 
 }
--- Proyecto 3/src/proyecto/pkg3/FXMLDocumentController.java
+++ Proyecto 3/src/proyecto/pkg3/FXMLDocumentController.java
@@ -12,6 +12,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import model.Cliente;
 
@@ -23,9 +24,17 @@ public class FXMLDocumentController implements Initializable {
     
     private ControlAgencia controlador = new ControlAgencia();
     
-    
-   
+     @FXML
+    private TableView<Cliente> tabla;
 
+    @FXML
+    private TableColumn<Cliente, String> nombreColumna = new TableColumn<Cliente, String>("NombreCompleto");
+
+    @FXML
+    private TableColumn<Cliente, Integer> cedulaColumna  = new TableColumn<Cliente, Integer>("NumeroIdentificacion");
+
+    @FXML
+    private TableColumn<Cliente, String> columnaTelefono  = new TableColumn<Cliente, String>("TelefonoContacto");
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -40,10 +49,24 @@ public class FXMLDocumentController implements Initializable {
         
     }   
     
-
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
     
-}
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {    
+        this.controlador = new ControlAgencia();
+        
+       fillItemsClientes();
+        tabla.getColumns().addAll();
+    }
+
+    private void fillItemsClientes() {
+        for(Cliente cln: controlador.getListaClientes().values()){
+            
+            //Llenar la tabla
+            tabla.getItems().add(cln);
+            
+            
+        }
+    }
+    
+    }
+  
